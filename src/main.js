3c18937350bacf501a7c0b8ea9cfe0170e185ff3
x-zhao-res <1025496505@qq.com>
2022-12-01 23:51:24 +0800

    feat:更新登陆、注册页面

--- src/main.js
+++ src/main.js
@@ -3,12 +3,17 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import G6 from "@antv/g6";
+import gsap from "gsap";
 import axios from "axios";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 Vue.config.productionTip = false;
+Vue.use(ElementUI);
+Vue.use(G6);
+Vue.use(axios);
+Vue.use(gsap);
 new Vue({
   router,
   store,
   render: (h) => h(App),
 }).$mount("#app");
-Vue.use(G6);
-Vue.use(axios);
---
+++ src/views/Login.vue
@@ -0,0 +1,272 @@
+<template>
+  <div>
+    <div class="login-border">
+      <div
+        ref="loginBox"
+        class="animate__animated animate__fadeIn animate__delay-1s login-box"
+      >
+        <div class="login-text">Nes-Work</div>
+        <div
+          class="input-username-box"
+          :style="{ marginBottom: register ? '20px' : '0px' }"
+        >
+          <div class="user-input">
+            <el-input placeholder="用户名" v-model="inF.username"></el-input>
+            <el-link
+              v-show="!register"
+              @click="toRegister"
+              style="user-select: none; color: #158bb8"
+              :underline="false"
+              type="primary"
+            >
+              没有账号?
+            </el-link>
+          </div>
+        </div>
+        <div class="input-password-box">
+          <div class="pass-input">
+            <el-input
+              placeholder="密码"
+              show-password
+              v-model="inF.password"
+            ></el-input>
+          </div>
+        </div>
+        <div v-if="register" class="input-repassword-box">
+          <div class="pass-input">
+            <el-input
+              placeholder="重复密码"
+              show-password
+              v-model="inF.repassword"
+            ></el-input>
+          </div>
+        </div>
+        <div v-if="register" class="input-repassword-box">
+          <div class="pass-input">
+            <el-input placeholder="手机号" v-model="inF.phoneNum"></el-input>
+          </div>
+        </div>
+        <div v-if="register" class="input-phonecode-box">
+          <div class="code-input">
+            <el-input
+              placeholder="手机验证码"
+              v-model="inF.password"
+            ></el-input>
+            <el-button style="width: 130px; margin-left: 10px">
+              发送验证码
+            </el-button>
+          </div>
+        </div>
+        <div v-if="!register" class="input-ip-box">
+          <div class="custom-input">
+            <el-input
+              placeholder="模拟器地址"
+              v-model="inF.customIp"
+            ></el-input>
+            <el-link
+              style="user-select: none; color: #158bb8"
+              :underline="false"
+              type="primary"
+            >
+              这是什么?
+            </el-link>
+          </div>
+        </div>
+        <div v-if="!register" class="custom-auth-box">
+          <div class="custom-auth-box-radio">
+            <el-radio-group v-model="radio">
+              <el-radio :label="0">GNS3</el-radio>
+              <el-radio :label="1">EvE-Ng</el-radio>
+              <el-radio :label="2">None</el-radio>
+            </el-radio-group>
+          </div>
+          <div class="custom-auth-box-text">选择模拟器并验证</div>
+        </div>
+        <div class="button-input">
+          <div v-if="!register" @click="login" class="button-login">登陆</div>
+          <div v-if="register" @click="registers" class="button-login">
+            注册
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "animate.css";
+// import { gsap } from "gsap";
+export default {
+  name: "Home",
+  data() {
+    return {
+      inF: {
+        username: "",
+        password: "",
+        customIp: "",
+        repassword: "",
+        phoneNum: "",
+      },
+      radio: "",
+      ceshi: false,
+      register: false,
+    };
+  },
+  methods: {
+    login() {},
+    registers() {
+      this.register = false;
+    },
+    toRegister() {
+      this.register = true;
+    },
+  },
+};
+</script>
+<style scoped lang="scss">
+.login-border {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background-color: #158bb8;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-box {
+    width: 25%;
+    height: auto;
+    background-color: #ffffff;
+    border-radius: 25px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    padding-top: 20px;
+    padding-bottom: 30px;
+    .login-text {
+      user-select: none;
+      width: 100%;
+      height: auto;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 30px;
+      font-family: 微软雅黑, serif;
+      font-weight: bolder;
+      color: #158bb8;
+    }
+    .input-username-box {
+      margin-top: 20px;
+      width: 100%;
+      height: auto;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .user-input {
+        width: 85%;
+        display: flex;
+        justify-content: start;
+        flex-wrap: wrap;
+      }
+    }
+    .input-password-box {
+      width: 100%;
+      height: auto;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .pass-input {
+        width: 85%;
+      }
+    }
+    .input-phonecode-box {
+      margin-top: 20px;
+      width: 100%;
+      height: auto;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .code-input {
+        width: 85%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: nowrap;
+      }
+    }
+    .input-repassword-box {
+      margin-top: 20px;
+      width: 100%;
+      height: auto;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .pass-input {
+        width: 85%;
+      }
+    }
+    .input-ip-box {
+      margin-top: 20px;
+      width: 100%;
+      height: auto;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-wrap: wrap;
+      .custom-input {
+        width: 85%;
+        height: auto;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: start;
+      }
+    }
+    .custom-auth-box {
+      margin-top: 10px;
+      width: 85%;
+      height: auto;
+      border: 2px dashed #158bb8;
+      border-radius: 10px;
+      padding-top: 5px;
+      padding-bottom: 5px;
+      .custom-auth-box-radio {
+        width: 100%;
+      }
+      .custom-auth-box-text {
+        margin-top: 5px;
+        width: 100%;
+        height: auto;
+        font-size: 15px;
+        font-family: 微软雅黑, serif;
+      }
+    }
+    .button-input {
+      width: 85%;
+      margin-top: 20px;
+      display: flex;
+      justify-content: center;
+      .button-login {
+        width: 50%;
+        height: 42px;
+        border: 2px solid #158bb8;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 18px;
+        color: #158bb8;
+        font-family: 微软雅黑, serif;
+        cursor: pointer;
+        transition-duration: 0.4s;
+        user-select: none;
+      }
+      .button-login:hover {
+        color: white;
+        background-color: #158bb8;
+        border: 2px solid white;
+      }
+    }
+  }
+}
+</style>
