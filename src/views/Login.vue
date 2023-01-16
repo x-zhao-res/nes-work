4053f892984e5b660abab28da6c6290101812fbf
x-zhao-res <1025496505@qq.com>
2023-01-16 22:38:30 +0800

    fix:重写选项页

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -9,58 +9,7 @@
         >
           <!--          // TODO 修改动画时间，让它看着舒服点 -->
           <div class="login-text">Nes-Work</div>
-          <div
-            class="input-username-box"
-            :style="{ marginBottom: register ? '20px' : '0px' }"
-          >
-            <div class="user-input">
-              <el-input placeholder="用户名" v-model="inF.username"></el-input>
-              <el-link
-                v-show="!register"
-                @click="toRegister"
-                style="user-select: none; color: #158bb8"
-                :underline="false"
-                type="primary"
-              >
-                没有账号?
-              </el-link>
-            </div>
-          </div>
-          <div class="input-password-box">
-            <div class="pass-input">
-              <el-input
-                placeholder="密码"
-                show-password
-                v-model="inF.password"
-              ></el-input>
-            </div>
-          </div>
-          <div v-if="register" class="input-repassword-box">
-            <div class="pass-input">
-              <el-input
-                placeholder="重复密码"
-                show-password
-                v-model="inF.repassword"
-              ></el-input>
-            </div>
-          </div>
-          <div v-if="register" class="input-repassword-box">
-            <div class="pass-input">
-              <el-input placeholder="手机号" v-model="inF.phoneNum"></el-input>
-            </div>
-          </div>
-          <div v-if="register" class="input-phonecode-box">
-            <div class="code-input">
-              <el-input
-                placeholder="手机验证码"
-                v-model="inF.password"
-              ></el-input>
-              <el-button style="width: 130px; margin-left: 10px">
-                发送验证码
-              </el-button>
-            </div>
-          </div>
-          <div v-if="!register" class="input-ip-box">
+          <div class="input-ip-box">
             <div class="custom-input">
               <el-input
                 placeholder="模拟器地址"
@@ -80,19 +29,13 @@
               <el-radio-group v-model="radio">
                 <el-radio :label="0">GNS3</el-radio>
                 <el-radio :label="1">EvE-Ng</el-radio>
-                <el-radio :label="2">None</el-radio>
               </el-radio-group>
             </div>
             <div class="custom-auth-box-text">选择模拟器并验证</div>
           </div>
           <div class="button-input">
-            <div v-if="!register" @click="login" class="button-login">登陆</div>
-            <div v-if="!register" @click="customLogin" class="button-login">
-              游客登陆
-            </div>
-            <div v-if="register" @click="registers" class="button-login">
-              注册
-            </div>
+            <div @click="login" class="button-login">登陆</div>
+            <div @click="customLogin" class="button-login">游客登陆</div>
           </div>
         </div>
       </div>
@@ -116,7 +59,6 @@ export default {
       },
       radio: "",
       ceshi: false,
-      register: false,
     };
   },
   methods: {
@@ -124,25 +66,6 @@ export default {
       this.$router.push("/item");
     },
     customLogin() {},
-    registers() {
-      //前往登陆页
-      this.register = false;
-    },
-    toRegister() {
-      //前往注册页
-      // gsap.to("#loginBox", {
-      //   duration: 1,
-      //   x: 200,
-      //   opacity: 0.5,
-      // });
-      this.register = true;
-      // this.register = true;
-      // gsap.to("#loginBox", {
-      //   duration: 1,
-      //   opacity: 1,
-      //   delay: 1,
-      // });
-    },
   },
 };
 </script>
@@ -205,7 +128,7 @@ export default {
         .user-input {
           width: 85%;
           display: flex;
-          justify-content: start;
+          justify-content: flex-start;
           flex-wrap: wrap;
         }
       }
@@ -266,7 +189,7 @@ export default {
           height: auto;
           display: flex;
           flex-wrap: wrap;
-          justify-content: start;
+          justify-content: flex-start;
         }
       }
 
--- src/views/itemChoose/index.vue
+++ src/views/itemChoose/index.vue
@@ -1,165 +1,16 @@
 <template>
-  <div>
-    <div class="choose-border">
-      <div class="left-border-box">
-        <div
-          @mouseover="menuCollacp = false"
-          id="menuBox"
-          class="nav-box-border"
-        >
-          <el-menu
-            class="el-menu-vertical-demo"
-            :collapse="menuCollacp"
-            active-text-color="#158bb8"
-            default-active="1"
-            :collapse-transition="true"
-            @select="componentChange"
-          >
-            <el-menu-item index="1">
-              <i style="font-size: 22px" class="el-icon-files"></i>
-              <span class="nav-item-text" slot="title">工程管理</span>
-            </el-menu-item>
-            <el-menu-item index="2">
-              <i style="font-size: 22px" class="el-icon-share"></i>
-              <span class="nav-item-text" slot="title">网络技术树</span>
-            </el-menu-item>
-            <el-menu-item index="3">
-              <i style="font-size: 22px" class="el-icon-present"></i>
-              <span class="nav-item-text" slot="title">镜像管理</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-              <i style="font-size: 22px" class="el-icon-setting"></i>
-              <span class="nav-item-text" slot="title">模拟器设置</span>
-            </el-menu-item>
-          </el-menu>
-        </div>
-      </div>
-      <div
-        class="show-box-border"
-        @mouseout="menuCollacp = true"
-        @mouseenter="menuCollacp = true"
-      >
-        <transition name="current-fade" mode="out-in">
-          <component :is="currentComponents"></component>
-        </transition>
-      </div>
-    </div>
-  </div>
+  <div></div>
 </template>
 
 <script>
-import itemChoose from "@/views/itemChoose/components/itemChoose";
-import mirrorManage from "@/views/itemChoose/components/mirrorManage";
-import networkTechTree from "@/views/itemChoose/components/networkTechTree";
-import simualSetting from "@/views/itemChoose/components/simualSetting";
 export default {
   name: "index.vue",
-  components: {
-    itemChoose,
-    mirrorManage,
-    networkTechTree,
-    simualSetting,
-  },
   mounted() {},
   data() {
-    return {
-      menuCollacp: true,
-      currentComponents: "itemChoose",
-    };
+    return {};
   },
-  methods: {
-    test() {
-      console.log(123);
-      this.menuCollacp = !this.menuCollacp;
-    },
-    componentChange(item) {
-      this.currentComponents =
-        item === "1"
-          ? "itemChoose"
-          : item === "2"
-          ? "networkTechTree"
-          : item === "3"
-          ? "mirrorManage"
-          : "simualSetting";
-    },
-  },
+  methods: {},
 };
 </script>
 
-<style scoped lang="scss">
-.current-fade-enter,
-.current-fade-leave-to {
-  opacity: 0;
-  transform: translateX(20px);
-}
-.current-fade-enter-active,
-.current-fade-leave-active {
-  transition: all 0.65s;
-}
-.v-enter,
-.v-leave-to {
-  opacity: 0;
-}
-.v-enter-active,
-.v-leave-active {
-  transition: all 0.42s;
-}
-.el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
-}
-.el-menu {
-  border-right: 0 !important;
-}
-.nav-item-text {
-  font-family: 微软雅黑, serif;
-  font-size: 16px;
-  font-weight: bold;
-}
-
-.choose-border {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  display: flex;
-  justify-content: flex-start;
-  align-content: center;
-  overflow-y: hidden;
-  //background-color: #548192;
-  background-color: #158bb8;
-  // TODO 从新选择所用背景
-  .left-border-box {
-    width: 20%;
-    height: 100%;
-    display: flex;
-    justify-content: flex-end;
-    align-content: flex-start;
-    .nav-box-border {
-      width: auto;
-      height: 50%;
-      border-radius: 15px;
-      padding: 15px 6px 20px;
-      box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 5px 0px,
-        rgba(0, 0, 0, 0.1) 0px 0px 1px 0px;
-      margin-top: 40px;
-      display: flex;
-      justify-content: center;
-      align-content: center;
-      transition: width 0.25s;
-      background-color: white;
-      .nav-menu {
-        transition: width 0.25s;
-      }
-    }
-  }
-  .show-box-border {
-    width: 60%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped lang="scss"></style>
